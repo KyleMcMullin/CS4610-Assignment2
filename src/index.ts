e18cefcf0905e951709270c8fa1a1e8681c9d060
KyleMcMullin <mcmullinjkyle@gmail.com>
2023-02-20 20:25:39 -0700

    working on authentication

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 const client = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 type Reptile = {
 
@@ -27,35 +28,34 @@ type CreateUserBody = {
 }
 
 type RequestWithSession = Request & {
-    session?: Session;
-    user?: User;
+  session?: Session
+  user?: User
 }
 
-
 const authenticationMiddleware: RequestHandler = async (req: RequestWithSession, res, next) => {
-    const sessionToken = req.cookies["session-token"];
-    if (sessionToken) {
-      const session = await client.session.findFirst({
-        where: {
-          token: sessionToken
-        },
-        include: {
-          user: true
-        }
-      });
-      if (session) {
-        req.session = session;
-        req.user = session.user;
+  const sessionToken = req.cookies["session-token"];
+  if (sessionToken) {
+    const session = await client.session.findFirst({
+      where: {
+        token: sessionToken
+      },
+      include: {
+        user: true
       }
+    });
+    if (session) {
+      req.session = session;
+      req.user = session.user;
     }
-    next();
   }
+  next();
+}
+
+app.use(authenticationMiddleware);
 
 //sign up
 app.post('/users', async (req, res) => {
   const {firstName, lastName, email, password} = req.body as CreateUserBody;
-  console.log(`password: ${password}`);
-  console.log(req.body);
   const passwordHash = await bcrypt.hash(req.body.password, 10);
   const user = await client.user.create({
     data: {
@@ -87,10 +87,57 @@ app.post('/users', async (req, res) => {
   res.json({ user });
 });
 
-  app.get("/", (req, res) => {
-    res.send(`<h1>Hello, world!</h1>`);
-  });
 
-  app.listen(3000, () => {
-    console.log("I got started!");
+type LoginBody = {
+  email: string,
+  password: string
+}
+
+// log in
+app.post("/sessions",  async (req, res) => {
+  const {email, password} = req.body as LoginBody;
+  const user = await client.user.findFirst({
+    where: {
+      email,
+    }
   });
+  if (!user) {
+    res.status(404).json({ message: "Invalid email or password" });
+    return;
+  }
+
+  const isValid = await bcrypt.compare(password, user.passwordHash);
+  if (!isValid) {
+    res.status(404).json({ message: "Invalid email or password" });
+    return;
+  }
+
+  const token = uuidv4();
+  const session = await client.session.create({
+    data: {
+      userId: user.id,
+      token,
+    }
+  })
+
+  res.cookie("session-token", session.token, {
+    httpOnly: true,
+    maxAge: 60000 * 10
+  })
+});
+
+app.get("/me", async (req: RequestWithSession, res) => {
+  if (req.session) {
+    res.json({ user: req.user });
+  } else {
+    res.status(401).json({ message: "unauthorized"});
+  }
+})
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello, world!</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log("I got started!");
+});
