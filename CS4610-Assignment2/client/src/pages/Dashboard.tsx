213c87ef7570817822a06473f62389d2e6cfeec1
curtisliu2 <92825247+curtisliu2@users.noreply.github.com>
2023-03-29 14:20:42 -0600

    Has errors needs fix

--- CS4610-Assignment2/client/src/pages/Dashboard.tsx
+++ CS4610-Assignment2/client/src/pages/Dashboard.tsx
@@ -3,28 +3,6 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { ApiContext } from "../contexts/api";
 import { useApi } from "../hooks/useApi";
 
-interface Reptile {
-    id: number;
-    name: string;
-}
-
-interface Schedule {
-    id: number,
-    reptileId: number,
-    userId: number,
-    type: string,
-    description: string,
-    monday: boolean,
-    tuesday: boolean,
-    wednesday: boolean,
-    thursday: boolean,
-    friday: boolean,
-    saturday: boolean,
-    sunday: boolean,
-    createdAt: Date,
-    updatedAt: Date
-}
-
 export const Dashboard = () => {
     const navigate = useNavigate();
     const api = useApi();
@@ -35,37 +13,30 @@ export const Dashboard = () => {
     const [newReptileSpecies, setNewReptileSpecies] = useState('');
     const [newReptileSex, setNewReptileSex] = useState('');
     const [reptileData, setReptileData] = useState([] as Reptile[])
-
-
-    const handleLogout = () => {
-        window.localStorage.removeItem("token");
-        navigate("/", { replace: true });
-    };
-
-    const handleAddReptile = () => {
-        const newReptile = {
-            id: Math.floor(Math.random() * 1000), // generate a random id
-            userId: userId, // assuming you have a variable named currentUserId that holds the current user's id
-            species: newReptileSpecies, // assuming you have a variable named newReptileSpecies that holds the new reptile's species
-            name: newReptileName,
-            sex: newReptileSex, // assuming you have a variable named newReptileSex that holds the new reptile's sex
-            createdAt: new Date(),
-            updatedAt: new Date()
-        };
-        // add the new reptile to the list of reptiles
-        setReptileData([...reptileData as Reptile[], newReptile as unknown as Reptile]);
-        // hide the add reptile modal
-        setShowAddReptileModal(false);
-        // reset the new reptile form inputs
-        setNewReptileName('');
-        setNewReptileSpecies('');
-        setNewReptileSex('');
-    }
-
-
+    
     useEffect(() => {
         getReptiles();
+        getSchedules();
     }, [])
+
+    type Schedule = {
+        id: number,
+        reptileId: number,
+        userId: number,
+        type: string,
+        description: string,
+        monday: boolean,
+        tuesday: boolean,
+        wednesday: boolean,
+        thursday: boolean,
+        friday: boolean,
+        saturday: boolean,
+        sunday: boolean,
+        createdAt: Date,
+        updatedAt: Date
+    }    
+
+
     type Reptile = {
         id: number,
         userId: number,
@@ -76,31 +47,55 @@ export const Dashboard = () => {
         updatedAt: Date
     }
 
-    function getReptiles(){
-        api.get('../reptile/' + reptileId)
-            .then(response => response.json())
-            .then(data => setReptileData(data as Reptile[]))
+   function getReptiles(){
+    api.get('../reptile/' + id)
+    .then(response => response.json())
+    .then(data => setReptileData(data as Reptile[]))
+}
+
+    function getSchedules(){
+        fetch('/schedules')
     }
 
+    const handleLogout = () => {
+        window.localStorage.removeItem("token");
+        navigate("/", { replace: true });
+    };
 
-    useEffect(() => {
-        api.get(`/${userId}/schedules`).then((response) => {
-            setSchedules(response.data.schedules);
-        });
-    }, []);
+    const handleAddReptile = () => {
+        const newReptile = {
+            id: reptileData.length + 1, // parse id to a number
+            userId: Number(userId),
+            species: newReptileSpecies,
+            name: newReptileName,
+            sex: newReptileSex,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+        // add the new reptile to the list of reptiles
+        setReptileData([...reptileData, newReptile]);
+        // hide the add reptile modal
+        setShowAddReptileModal(false);
+        // reset the new reptile form inputs
+        setNewReptileName('');
+        setNewReptileSpecies('');
+        setNewReptileSex('');
+    };
 
     return (
         <div>
-            <h2>Welcome to your Reptile Dashboard:</h2>
+            <h2>Welcome to your Dashboard:</h2>
 
             <h3>List of Reptiles:</h3>
             <ul>
-                {reptileData.map((reptile) => (
-                    <li key={reptile?.id}>
-                        <Link to={`/reptile/${reptile?.id}`}>{reptile?.name}</Link>
+                {reptileData && reptileData.map((reptile) => (
+                    <li key={reptile.id}>
+                        {/* <button onClick={() => navigate('../reptile/' + id + '/' + reptile.id, {replace: true})}>{reptile.name}</button> */}
+                        <button onClick={() => navigate('../reptile/' + reptile.id, {replace: true})}>{reptile.name}</button>
+
                     </li>
                 ))}
-            </ul>
+            </ul>   
 
             <button onClick={() => setShowAddReptileModal(true)}>Add Reptile</button>
 
@@ -119,27 +114,6 @@ export const Dashboard = () => {
                     <button onClick={handleAddReptile}>Add</button>
                 </div>
             )}
-
-            {/* <h3>List of Schedules:</h3>
-            <ul>
-                {schedules.map((schedule) => (
-                    <li key={schedule.id}>
-                        <p>Reptile Id: {schedule.reptileId}</p>
-                        <p>Type: {schedule.type}</p>
-                        <p>Description: {schedule.description}</p>
-                        <p>Days:</p>
-                        <ul>
-                            <li>Monday: {schedule.monday ? "Yes" : "No"}</li>
-                            <li>Tuesday: {schedule.tuesday ? "Yes" : "No"}</li>
-                            <li>Wednesday: {schedule.wednesday ? "Yes" : "No"}</li>
-                            <li>Thursday: {schedule.thursday ? "Yes" : "No"}</li>
-                            <li>Friday: {schedule.friday ? "Yes" : "No"}</li>
-                            <li>Saturday: {schedule.saturday ? "Yes" : "No"}</li>
-                            <li>Sunday: {schedule.sunday ? "Yes" : "No"}</li>
-                        </ul>
-                    </li>
-                ))}
-            </ul> */}
 
             <button onClick={handleLogout}>Logout</button>
         </div>
