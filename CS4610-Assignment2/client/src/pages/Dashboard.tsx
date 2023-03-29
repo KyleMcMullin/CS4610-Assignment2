7dfd535fe248167a1d3db708ca5671fe56970e63
curtisliu2 <92825247+curtisliu2@users.noreply.github.com>
2023-03-29 12:35:22 -0600

    Dashboard base

--- CS4610-Assignment2/client/src/pages/Dashboard.tsx
+++ CS4610-Assignment2/client/src/pages/Dashboard.tsx
@@ -1,112 +1,138 @@
-import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ApiContext } from "../contexts/api";
+import { useApi } from "../hooks/useApi";
 
-type Reptile = {
+interface Reptile {
     id: number;
-    userId: number;
-    species: string;
     name: string;
-    sex: string;
-    createdAt: Date;
-    updatedAt: Date;
-};
+    // Add other properties here
+}
 
-type Schedule = {
-    id: number;
-    reptileId: number;
-    userId: number;
-    type: string;
-    description: string;
-    monday: boolean;
-    tuesday: boolean;
-    wednesday: boolean;
-    thursday: boolean;
-    friday: boolean;
-    saturday: boolean;
-    sunday: boolean;
-    createdAt: Date;
-    updatedAt: Date;
-};
+interface Schedule {
+    id: number,
+    reptileId: number,
+    userId: number,
+    type: string,
+    description: string,
+    monday: boolean,
+    tuesday: boolean,
+    wednesday: boolean,
+    thursday: boolean,
+    friday: boolean,
+    saturday: boolean,
+    sunday: boolean,
+    createdAt: Date,
+    updatedAt: Date
+}
 
-const Dashboard = () => {
+export const Dashboard = () => {
+    const navigate = useNavigate();
+    const api = useApi();
+    const [firstName, setFirstName] = useState("");
     const [reptiles, setReptiles] = useState<Reptile[]>([]);
+    const [showAddReptileModal, setShowAddReptileModal] = useState(false);
+    const [newReptileName, setNewReptileName] = useState("");
     const [schedules, setSchedules] = useState<Schedule[]>([]);
-    const history = useHistory();
+
+    const handleLogout = () => {
+        window.localStorage.removeItem("token");
+        navigate("/", { replace: true });
+    };
+
+    const handleAddReptile = async () => {
+        const userId = window.localStorage.getItem("userId");
+        const response = await api.post(`/reptile/${userId}`, { name: newReptileName });
+        const newReptile = response.data;
+        setReptiles([...reptiles, newReptile]);
+        setShowAddReptileModal(false);
+        setNewReptileName("");
+    };
 
     useEffect(() => {
-        const fetchReptiles = async () => {
-            try {
-                const response = await fetch("/reptiles");
-                const data = await response.json();
+        const userId = window.localStorage.getItem("userId");
+        api.get(`/reptile/${userId}`).then((response) => {
+            response.json().then((data: Reptile[]) => {
                 setReptiles(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        const fetchSchedules = async () => {
-            const day = new Date().toLocaleString('en-us', { weekday: 'long' }).toLowerCase();
-            try {
-                const response = await fetch(`/schedules/${day}`);
-                const data = await response.json();
-                setSchedules(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchReptiles();
-        fetchSchedules();
+            });
+        });
     }, []);
 
-    const handleAddReptile = () => {
-        history.push("/add-reptile");
-    };
-
-    const handleEditReptile = (id: number) => {
-        history.push(`/edit-reptile/${id}`);
-    };
-
-    const handleDeleteReptile = async (id: number) => {
-        try {
-            await fetch(`/reptiles/${id}`, {
-                method: 'DELETE',
-            });
-            setReptiles(reptiles.filter((reptile) => reptile.id !== id));
-        } catch (error) {
-            console.error(error);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await api.get("/users/current"); // make API call to get current user's data
+                setFirstName(response.data.firstName); // set the first name in the state
+            } catch (e) {
+                console.log(e);
+            }
         }
-    };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const userId = window.localStorage.getItem("userId");
+        api.get(`/${userId}/schedules`).then((response) => {
+            setSchedules(response.data.schedules);
+        });
+    }, []);
+
+    // #TODO Uncomment when you figure out token issues
+
+    // useEffect(() => {
+    //     if (!window.localStorage.getItem("token")) {
+    //         navigate("/", {
+    //             replace: true
+    //         })
+    //     }
+    // }, [])
 
     return (
         <div>
-            <h1>Dashboard</h1>
-            <button onClick={handleAddReptile}>Add Reptile</button>
-            {reptiles.map((reptile) => (
-                <div key={reptile.id}>
-                    <h2>{reptile.name}</h2>
-                    <p>Species: {reptile.species}</p>
-                    <p>Sex: {reptile.sex}</p>
-                    <button onClick={() => handleEditReptile(reptile.id)}>Edit</button>
-                    <button onClick={() => handleDeleteReptile(reptile.id)}>Delete</button>
+            <h2>Welcome to {firstName} Dashboard:</h2>
+
+            <h3>List of Reptiles:</h3>
+            <ul>
+                {reptiles.map((reptile) => (
+                    <li key={reptile?.id}>
+                        <Link to={`/reptile/${reptile?.id}`}>{reptile?.name}</Link>
+                    </li>
+                ))}
+            </ul>
+
+            <button onClick={() => setShowAddReptileModal(true)}>Add Reptile</button>
+
+            {showAddReptileModal && (
+                <div>
+                    <label htmlFor="newReptileName">Name:</label>
+                    <input type="text" id="newReptileName" value={newReptileName} onChange={(e) => setNewReptileName(e.target.value)} />
+                    <button onClick={handleAddReptile}>Add</button>
                 </div>
-            ))}
-            <h2>Schedule for today</h2>
-            {schedules.length > 0 ? (
-                schedules.map((schedule) => (
-                    <div key={schedule.id}>
-                        <h3>{schedule.type}</h3>
-                        <p>{schedule.description}</p>
-                        <p>Monday: {schedule.monday ? "Yes" : "No"}</p>
-                        <p>Tuesday: {schedule.tuesday ? "Yes" : "No"}</p>
-                        <p>Wednesday: {schedule.wednesday ? "Yes" : "No"}</p>
-                        <p>Thursday: {schedule.thursday ? "Yes" : "No"}</p>
-                        <p>Friday: {schedule.friday ?"Yes" : "No"}</p>
-                        <p>Saturday: {schedule.saturday ? "Yes" : "No"}</p>
-                        <p>Sunday: {schedule.sunday ? "Yes" : "No"}</p>
-                    </div>
-                )))}
+            )}
+
+            <h3>List of Schedules:</h3>
+            <ul>
+                {schedules.map((schedule) => (
+                    <li key={schedule.id}>
+                        <p>Reptile Id: {schedule.reptileId}</p>
+                        <p>Type: {schedule.type}</p>
+                        <p>Description: {schedule.description}</p>
+                        <p>Days:</p>
+                        <ul>
+                            <li>Monday: {schedule.monday ? "Yes" : "No"}</li>
+                            <li>Tuesday: {schedule.tuesday ? "Yes" : "No"}</li>
+                            <li>Wednesday: {schedule.wednesday ? "Yes" : "No"}</li>
+                            <li>Thursday: {schedule.thursday ? "Yes" : "No"}</li>
+                            <li>Friday: {schedule.friday ? "Yes" : "No"}</li>
+                            <li>Saturday: {schedule.saturday ? "Yes" : "No"}</li>
+                            <li>Sunday: {schedule.sunday ? "Yes" : "No"}</li>
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+
+            <button onClick={handleLogout}>Logout</button>
         </div>
     );
-};
-export default Dashboard;
+}
